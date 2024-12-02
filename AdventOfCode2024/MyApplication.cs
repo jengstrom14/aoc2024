1d763dfa4f52f5dfc79f2aeaa75d2321fef89e49
Jackson Engstrom <engstromjackson@gmail.com>
2024-12-01 18:28:25 -0800

    day 1

--- AdventOfCode2024/MyApplication.cs
+++ AdventOfCode2024/MyApplication.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -20,6 +21,101 @@ namespace AdventOfCode2024
         public async Task RunProgram()
         {
             _logger.LogInformation("Running Program");
+            var list1 = new List<int>();
+            var list2 = new List<int>();
+
+            var lines = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "day1-input.txt"));
+
+            foreach (var line in lines)
+            {
+                var split = line.Split();
+                list1.Add(int.Parse(split[0]));
+                list2.Add(int.Parse(split[3]));
+            }
+            list1.Sort();
+            list2.Sort();
+
+            var simscoreByNum = new Dictionary<int, int>();
+            int totalSimScore = 0;
+
+            foreach (var number in list1)
+            {
+                if (simscoreByNum.TryGetValue(number, out var simScore))
+                {
+                    totalSimScore += simScore;
+                    continue;
+                }
+                var count = list2.Where(x => x == number).Count();
+                simScore = number * count;
+                simscoreByNum[number] = simScore;
+                totalSimScore += simScore;
+            }
+
+            Console.WriteLine(totalSimScore);
+        }
+
+        public async Task Day1Part2()
+        {
+            _logger.LogInformation("Running Program");
+            var list1 = new List<int>();
+            var list2 = new List<int>();
+
+            var lines = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "day1-input.txt"));
+
+            foreach (var line in lines)
+            {
+                var split = line.Split();
+                list1.Add(int.Parse(split[0]));
+                list2.Add(int.Parse(split[3]));
+            }
+            list1.Sort();
+            list2.Sort();
+
+            var simscoreByNum = new Dictionary<int, int>();
+            int totalSimScore = 0;
+
+            foreach (var number in list1)
+            {
+                if (simscoreByNum.TryGetValue(number, out var simScore))
+                {
+                    totalSimScore += simScore;
+                    continue;
+                }
+                var count = list2.Where(x => x == number).Count();
+                simScore = number * count;
+                simscoreByNum[number] = simScore;
+                totalSimScore += simScore;
+            }
+
+            Console.WriteLine(totalSimScore);
+        }
+
+        public async Task Day1Part1()
+        {
+            _logger.LogInformation("Running Program");
+            var list1 = new List<int>();
+            var list2 = new List<int>();
+
+            var lines = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "day1-input.txt"));
+
+            foreach (var line in lines)
+            {
+                var split = line.Split();
+                list1.Add(int.Parse(split[0]));
+                list2.Add(int.Parse(split[3]));
+            }
+            list1.Sort();
+            list2.Sort();
+
+            var totalDiff = 0;
+
+            for(int i = 0; i < list1.Count; i++)
+            {
+                var difference = Math.Abs(list1[i] - list2[i]);
+                totalDiff += difference;
+            }
+
+            Console.WriteLine(totalDiff);
         }
     }
 }
